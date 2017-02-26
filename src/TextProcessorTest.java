e57ceb83025749116604601db94d3cfeddfff6c4
Fred Zhang <fredzqm@gmail.com>
2017-02-26 04:00:06 -0500

    add random testing

--- src/TextProcessorTest.java
+++ src/TextProcessorTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 import org.junit.Test;
 
 public class TextProcessorTest {
@@ -31,6 +35,63 @@ public class TextProcessorTest {
 		assertTrue(t.contains("abb"));
 		assertTrue(t.contains("aabb"));
 		assertEquals(8, t.getCount());
+	}
+
+	@Test
+	public void test3() {
+		runRandomTest(2);
+		runRandomTest(20);
+		runRandomTest(60);
+		runRandomTest(70);
+		runRandomTest(100);
+		runRandomTest(200);
+	}
+	
+	private void runRandomTest(int length) {
+		String str = getRandomString(length);
+		Set<String> substrs = getSet(str, 0, str.length());
+		SuffixTree tree = new SuffixTree();
+		tree.addString(str);
+		assertEquals(substrs.size(), tree.getCount());
+		for (String s : substrs) {
+			tree.contains(s);
+		}
+	}
+	
+	private String getRandomString(int length) {
+		Random r = new Random();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < length; i++)
+			sb.append(r.nextInt(length));
+		return sb.toString();
+	}
+
+	private void runTest(String str, int W, int[] ques) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(ques[0]);
+		for (int i = 1; i < ques.length; i++)
+			sb.append(", " + ques[i]);
+		System.out.printf("String: \"%s\"\nW: %d\nques: new int[]{%s}\n\n", str, W, sb);
+
+		for (int qi = 0; qi < ques.length; qi++) {
+			int start = ques[qi] - 1;
+			int end = start + W;
+			start = Math.max(start, 0);
+			end = Math.min(end, str.length());
+
+			getSet(str, start, end);
+		}
+	}
+
+
+	private Set<String> getSet(String str, int start, int end) {
+		Set<String> set = new HashSet<>();
+		for (int i = start; i < end; i++) {
+			for (int j = i + 1; j <= end; j++) {
+				set.add(str.substring(i, j));
+			}
+		}
+		return set;
 	}
 
 }
